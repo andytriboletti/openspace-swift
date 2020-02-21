1c58b6591db965d1816c22826670249b96930b61
Andy Triboletti <andy.triboletti@gmail.com>
2020-02-20 22:55:58 -0500

    flag and mars

--- Open Space/ViewControllers/MarsViewController.swift
+++ Open Space/ViewControllers/MarsViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 import MaterialComponents.MaterialButtons
 import MaterialComponents.MaterialButtons_Theming
+import SceneKit
 
 class MarsViewController: UIViewController {
     @IBOutlet var takeOffButton: MDCButton!
+    
+    var baseNode:SCNNode!
+    @IBOutlet var scnView: SCNView!
 
+    
     @IBAction func takeOffAction() {
         self.performSegue(withIdentifier: "takeOff", sender: self)
     }
@@ -24,6 +29,69 @@ class MarsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        baseNode = SCNNode()
+        let scene = SCNScene()
+
+        let backgroundFilename = "PIA01120orig.jpg"
+        let image = UIImage(named: backgroundFilename)!
+        
+        let size = CGSize(width: self.view.frame.width, height: self.view.frame.height)
+        let aspectScaledToFitImage = image.af_imageAspectScaled(toFill: size)
+        scene.background.contents = aspectScaledToFitImage
+        scene.background.wrapS = SCNWrapMode.repeat
+        scene.background.wrapT = SCNWrapMode.repeat
+                
+        //addObject(name: "space11.dae", position: nil, scale: SCNVector3(10.0,10.0,10.0))
+        
+        scene.rootNode.addChildNode(baseNode)
+
+        
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        // retrieve the ship node
+        //let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        
+        // animate the 3d object
+        //ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+        
+        // retrieve the SCNView
+        let scnView = self.scnView!
+        //self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = false
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.black
+        
+        
+        
         self.takeOffButton.applyTextTheme(withScheme: appDelegate.containerScheme)
         self.takeOffButton.applyContainedTheme(withScheme: appDelegate.containerScheme)
         
