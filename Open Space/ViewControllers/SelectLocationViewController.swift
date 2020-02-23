b9a4b13e54974783dff4f32508c468c910d94506
Andy Triboletti <andy.triboletti@gmail.com>
2020-02-22 19:06:33 -0500

    select location updates

--- Open Space/ViewControllers/SelectLocationViewController.swift
+++ Open Space/ViewControllers/SelectLocationViewController.swift
@@ -7,21 +7,26 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
 class SelectLocationViewController: AlertViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     @IBOutlet var collectionView: UICollectionView!
     @IBOutlet var collectionViewFlowLayout: UICollectionViewFlowLayout!
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return 3
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         1
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "locationIdentifier", for: indexPath)// as! LocationCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "locationIdentifier", for: indexPath) as! LocationCollectionViewCell
         cell.backgroundColor = .green
-        //cell.cellImage.image = UIImage(named: "rocket_1024.png")
+        let cellImage = UIImage(named: "rocket_1024.png")
+        let size = CGSize(width: 230, height: 230)
+        let aspectScaledToFitImage = cellImage?.af_imageAspectScaled(toFill: size)
+        cell.cellImage.image = aspectScaledToFitImage
         return cell
     }
     
@@ -30,22 +35,9 @@ class SelectLocationViewController: AlertViewController, UICollectionViewDataSou
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.automaticallyAdjustsScrollViewInsets = false
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
         self.collectionViewFlowLayout.itemSize = CGSize(width: 128, height: 128)
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
