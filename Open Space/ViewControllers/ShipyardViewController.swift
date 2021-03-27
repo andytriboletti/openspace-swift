bc8ab9cacaf6688dd0c46dea46d9fa8a2f7eec7f
Andy Triboletti <andy.triboletti@gmail.com>
2021-03-27 13:06:09 -0400

    Start of shipyard

---
+++ Open Space/ViewControllers/ShipyardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShipyardViewController.swift
+//  Open Space
+//
+//  Created by Andy Triboletti on 3/27/21.
+//  Copyright © 2021 GreenRobot LLC. All rights reserved.
+//
+
+import UIKit
+
+class ShipyardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
