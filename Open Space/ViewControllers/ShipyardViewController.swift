f9b980c9aca1263f98345fa4a32fb45d090f75e8
Andy Triboletti <andy.triboletti@gmail.com>
2021-03-31 17:36:27 -0400

    Add IBActions for buttons

--- Open Space/ViewControllers/ShipyardViewController.swift
+++ Open Space/ViewControllers/ShipyardViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ShipyardViewController: UIViewController {
 
+    @IBAction func configureShipName() {
+        print("configure ship name")
+    }
+    
+    @IBAction func configureShipDesign() {
+        print("configure ship design")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
