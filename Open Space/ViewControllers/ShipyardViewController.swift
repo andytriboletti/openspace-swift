0561ddff93403b1b0a1536229c1b222bc3d994a1
Andy Triboletti <andy.triboletti@gmail.com>
2021-04-28 17:06:09 -0400

    configure ship name

--- Open Space/ViewControllers/ShipyardViewController.swift
+++ Open Space/ViewControllers/ShipyardViewController.swift
@@ -12,6 +12,23 @@ class ShipyardViewController: UIViewController {
 
     @IBAction func configureShipName() {
         print("configure ship name")
+        //1. Create the alert controller.
+        let alert = UIAlertController(title: "Some Title", message: "Enter a text", preferredStyle: .alert)
+
+        //2. Add the text field. You can configure it however you need.
+        alert.addTextField { (textField) in
+            textField.text = "Some default text"
+        }
+
+        // 3. Grab the value from the text field, and print it when the user clicks OK.
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+            print("Text field: \(textField!.text)")
+        }))
+
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+        
     }
     
     @IBAction func configureShipDesign() {
