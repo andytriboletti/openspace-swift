b722a083f8d108136a8e64a4fc52157dc356dce4
Andy Triboletti <andy.triboletti@gmail.com>
2024-06-16 15:15:45 -0400

    Start of Configure SpaceStation

---
+++ Open Space/ViewControllers/Earth/ConfigureSpaceStationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ConfigureSpaceStationViewController.swift
+//  Open Space
+//
+//  Created by Andrew Triboletti on 6/16/24.
+//  Copyright © 2024 GreenRobot LLC. All rights reserved.
+//
+
+import UIKit
+
+class ConfigureSpaceStationViewController: UIViewController {
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
